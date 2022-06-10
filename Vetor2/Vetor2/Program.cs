574f210a1055b8c230e28a8a9811245fbb9fe62f
Gabriel Lima <75463912+gabb-z@users.noreply.github.com>
2022-06-09 23:10:36 -0300

    Vetor2 - Soma e Média dos Elementos

---
+++ Vetor2/Vetor2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+
+namespace Vetor2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;
+            double[] vetor;
+
+            N = int.Parse(Console.ReadLine());
+            vetor = new double[N];
+            string[] s = Console.ReadLine().Split(' ');
+
+            for (int i = 0; i < N; i++)
+            {
+                vetor[i] = double.Parse(s[i], CultureInfo.InvariantCulture);
+            }
+
+            for (int i = 0; i < N; i++)
+            {
+                Console.Write(vetor[i].ToString("F1", CultureInfo.InvariantCulture) + " ");
+            }
+
+            Console.WriteLine();
+
+            double soma = 0.0;
+
+            for (int i = 0; i < N; i++)
+            {
+                soma += vetor[i];
+            }
+
+            double media = soma / N;
+
+            Console.WriteLine(soma.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine(media.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
